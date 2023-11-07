daa3a9cbc2e5d245156dcb90a1476cbef6814dc0
yogi8247 <yogimuddada1@gmail.com>
2023-11-07 09:50:29 +0530

    doe

---
+++ src/main/java/cse_lab/UserValidation.java
@@ -0,0 +1,17 @@
+package cse_lab;
+
+import java.util.ResourceBundle;
+
+public class UserValidation {
+	public boolean check(String inUID,String inPSW)
+	{
+		ResourceBundle rb=ResourceBundle.getBundle("config");
+		String UID=rb.getString("uname");
+		String PSW=rb.getString("psw");
+		if(inUID.equals(UID)&&inPSW.equals(PSW))
+			return true;
+		else
+			return false;
+	}
+
+}
---
+++ src/test/java/cse_lab/UserValidationTest.java
@@ -0,0 +1,5 @@
+package cse_lab;
+
+public class UserValidationTest {
+
+}
